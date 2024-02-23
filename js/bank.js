8ba6eb332c75f704d6b96f0f966f44067bcde7e5
Nur Hossain <nurhossain.webd@gmail.com>
2024-02-23 10:50:53 +0100

    transaction page redirected

---
+++ js/bank.js
@@ -0,0 +1,10 @@
+document.getElementById('login').addEventListener('click', function () {
+    const emailfield = document.getElementById('email');
+    const emailid = emailfield.value;
+    const passfield = document.getElementById('password');
+    const password = passfield.value;
+    if (emailid == 'simple@gmail.de' && password == 1234) {
+        window.location.href = 'bank.html';
+    }
+
+})
